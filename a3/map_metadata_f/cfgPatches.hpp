11f02315a78ae5722dd4d7e8b932b4abc6d423c8
Tom_48_97 ☠ <tom_48_97@me.com>
2019-08-03 19:13:27 +0200

    Added: Addon configuration Added: Area categories (macros)

---
+++ a3/map_metadata_f/cfgPatches.hpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+	class A3_Map_Metadata
+	{
+		author = CFGPATCHES_AUTHOR;
+		name = CFGPATCHES_A3_MAP_METADATA;
+		url = "https://www.arma3.com";
+		units[] = {};
+		weapons[]={};
+		requiredVersion = 1.78;
+		requiredAddons[] =
+		{
+		};
+	};
+};
---
+++ a3/map_metadata_f/config.cpp
@@ -0,0 +1,2 @@
+#include "\A3\basicDefines_A3.hpp"
+#include "cfgPatches.hpp"
